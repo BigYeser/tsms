c04ddf69abd4141f757cb9e073ad24d12a43ca70
MACOOK <macook@Raqmi-plus-abdullah.local>
2022-11-25 16:46:23 +0400

    demo to change create orderSevice after build8

--- resources/js/Pages/Orders/Create.vue
+++ resources/js/Pages/Orders/Create.vue
@@ -230,7 +230,7 @@
                   <tfoot>
                     <tr>
                       <th class="border-b px-4 py-2 bg-gray-200 ltr:text-right rtl:text-left" colspan="6">{{ $t('Total') }}</th>
-                      <th></th>
+                      <th class="border-b px-4 py-2 bg-gray-200 ltr:text-right rtl:text-left"></th>
                       <th class="border-b px-4 py-2 bg-gray-200 text-center">
                         {{ $number(totalQuantity) }}
                       </th>
@@ -240,7 +240,7 @@
                     </tr>
                     <tr v-if="form.discount">
                       <th class="border-b px-4 py-2 bg-gray-200 ltr:text-right rtl:text-left" colspan="6">{{ $t('Discount') }}</th>
-                      <th></th>
+                      <th class="border-b px-4 py-2 bg-gray-200 ltr:text-right rtl:text-left"></th>
                       <th class="border-b px-4 py-2 bg-gray-200 ltr:text-right rtl:text-left">
                         {{ $number(discountAmount) }}
                       </th>
@@ -248,14 +248,14 @@
                     <template v-if="form.taxes.length">
                       <tr v-for="(tax, ti) in nonCompoundTaxes" :key="'tr_' + ti">
                         <th class="border-b px-4 py-2 bg-gray-200 ltr:text-right rtl:text-left" colspan="6">{{ tax.label }}</th>
-                        <th></th>
+                        <th class="border-b px-4 py-2 bg-gray-200 ltr:text-right rtl:text-left"></th>
                         <th class="border-b px-4 py-2 bg-gray-200 ltr:text-right rtl:text-left">
                           {{ $number(calculateTax(tax, false)) }}
                         </th>
                       </tr>
                       <tr v-for="(tax, ti) in compoundTaxes" :key="'ctr_' + ti">
                         <th class="border-b px-4 py-2 bg-gray-200 ltr:text-right rtl:text-left" colspan="6">{{ tax.label }}</th>
-                        <th></th>
+                        <th class="border-b px-4 py-2 bg-gray-200 ltr:text-right rtl:text-left"></th>
                         <th class="border-b px-4 py-2 bg-gray-200 ltr:text-right rtl:text-left">
                           {{ $number(calculateTax(tax, true)) }}
                         </th>
@@ -263,7 +263,7 @@
                     </template>
                     <tr v-if="form.taxes.length || form.discount">
                       <th class="border-b px-4 py-2 bg-gray-200 ltr:text-right rtl:text-left" colspan="6">{{ $t('Grand Total') }}</th>
-                      <th></th>
+                      <th class="border-b px-4 py-2 bg-gray-200 ltr:text-right rtl:text-left"></th>
                       <th class="border-b px-4 py-2 bg-gray-200 ltr:text-right rtl:text-left">
                         {{ $number(totalAmount - discountAmount + taxAmount) }}
                       </th>
