a3218a75dd7365e42f31bf9d5e4de7437a985e64
MACOOK <macook@Raqmi-plus-abdullah.local>
2022-11-26 11:19:52 +0400

    index measurmetn fix to show coustemrs name and sevice name

--- resources/js/Pages/Measurements/Index.vue
+++ resources/js/Pages/Measurements/Index.vue
@@ -51,7 +51,6 @@
             </td> -->
             <td class="border-t px-6 py-4 w-48">
               <div v-if="measurement.customer_id">
-                
                 {{ getCustomerName(measurement)}}
               </div>
             </td>
@@ -167,7 +166,8 @@ export default {
       fetch(this.route('measurements.show', [measurement.id, md5(measurement.name)]) + '?ajax=1').then(res => {
         res.json().then(data => {
             console.log(data);
-            return data.customer;
+            console.log(customer.name);
+            return data.customer.name;
         });
       }); 
     },
