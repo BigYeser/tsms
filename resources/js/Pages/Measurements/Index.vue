06f0745aca6de3f4ffac98e071781047280f9061
MACOOK <macook@Raqmi-plus-abdullah.local>
2022-11-26 10:42:29 +0400

    index measurmetn fix to show coustemrs name and sevice name

--- resources/js/Pages/Measurements/Index.vue
+++ resources/js/Pages/Measurements/Index.vue
@@ -50,7 +50,7 @@
               </div>
             </td>
             <td class="border-t px-6 py-4 w-48">
-              {{route('measurements.show',measurement.id,md5(measurement.name))}}
+              {{route('measurements.show',[measurement.id,md5(measurement.name)])}}
               <div v-if="measurement.customer_id">
                 {{ measurement.customer }}
               </div>
