0f6788c0e67bbd2e1e43c69f14e06cd9b6b28743
Abd Albary Tarakji <abd.tarakji96@gmail.com>
2022-11-30 04:08:46 +0400

    fix order show page

--- resources/js/Jetstream/Modal.vue
+++ resources/js/Jetstream/Modal.vue
@@ -2,7 +2,7 @@
   <teleport to="body">
     <div
       v-show="show"
-      class="fixed z-10 inset-0 overflow-y-auto w-full"
+      class="fixed z-10 inset-0 overflow-y-auto"
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
